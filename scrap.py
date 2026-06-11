9836c6069720849f6b04b985802f3e90ca46e516
amin10003 <maxamedxasancali10003@gmail.com>
2026-06-11 19:20:45 +0300

    extract quote and auther from https://quotes.toscrape.com

---
+++ scrap.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_quote():
+    url = "https://quotes.toscrape.com"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    quotes = soup.find_all("div", class_="quote")
+
+    # print(quotes)
+    for qoute in quotes:
+        motivation = qoute.span.text
+        auther = qoute.small.text
+
+        print(f"Motivation: {motivation}\n Auther: {auther}")
+
+
+get_quote()
