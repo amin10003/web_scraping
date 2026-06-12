da55412da861d2626a2672078204b4a67bac480a
amin10003 <maxamedxasancali10003@gmail.com>
2026-06-12 11:55:57 +0300

    add pandas for better  data structure

--- scrap.py
+++ scrap.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
+show_scraped_data = []
 
 def get_quote():
     url = "https://quotes.toscrape.com"
@@ -13,7 +15,22 @@ def get_quote():
         motivation = qoute.span.text
         auther = qoute.small.text
 
-        print(f"Motivation: {motivation}\nAuther: {auther}")
+        # print(f"Motivation: {motivation}\nAuther: {auther}")
+        
+        quot_dic = {
+            "Author": auther,
+            "Qoute": motivation
+        }
+        show_scraped_data.append(quot_dic)
+        df = pd.DataFrame(show_scraped_data)
+        df["Qoute"] = df["Qoute"].str.replace('""', ' ')
+        print(df)
 
 
 get_quote()
+
+
+
+
+
+
